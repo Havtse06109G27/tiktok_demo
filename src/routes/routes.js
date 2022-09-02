9a4232bbc4e094f61e49a67b59ecbbf4c5d0e40b
havtse06109 <havtse06109@gmail.com>
2022-09-02 15:20:56 +0700

    Build UI for side bar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,21 @@
+import config from '~/config';
+
 // Layout
 import { HeaderOnly } from '~/layouts';
 
+// Pages
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
-import config from '~/config';
+import Live from '~/pages/Live';
 
 // Những trang không cần đăng nhập vẫn xem được
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
